b6f010747a45a7752761a51c4680be26c8430ac5
dwfncar <fillmore@ucar.edu>
2024-05-11 10:35:03 -0600

    Added start and end command line args.

--- scripts/spatial_spectral_optics.py
+++ scripts/spatial_spectral_optics.py
@@ -2,9 +2,11 @@ import os
 import sys
 import argparse
 import logging
+import numpy as np
+import pandas as pd
 
 if __name__ == '__main__':
-        
+
     """
     Parse command line arguments
     """     
@@ -14,6 +16,18 @@ if __name__ == '__main__':
         help='log file (default stdout)')
     parser.add_argument('--debug', action='store_true',
         help='set logging level to debug')
+    parser.add_argument('--datadir', type=str,
+        default=os.path.join(os.getenv('HOME'), 'Data'),
+        help='top-level data directory (default $HOME/Data)')
+    parser.add_argument('--start', type=str,
+        default='20080701',
+        help='start date (yyyymmdd)')
+    parser.add_argument('--end', type=str,
+        default='20080701',
+        help='end date (yyyymmdd)')
+    parser.add_argument('--file_pattern', type=str,
+        default=os.path.join('MERRA2_inst3', 'aer_Nv',
+            'MERRA2_300.inst3_3d_aer_Nv.yyyymmdd.nc4'))
     args = parser.parse_args()
 
     """
@@ -21,4 +35,7 @@ if __name__ == '__main__':
     """
     logging_level = logging.DEBUG if args.debug else logging.INFO
     logging.basicConfig(stream=args.logfile, level=logging_level)
+
+    dates = pd.date_range(start=args.start, end=args.end, freq='D')
+    logging.info(dates)
 
