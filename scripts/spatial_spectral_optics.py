d62efd8e2d8d68310d82daff5309c0f4e0a7e85e
dwfncar <fillmore@ucar.edu>
2024-05-12 14:27:52 -0600

    Read aerosol yaml.

--- scripts/spatial_spectral_optics.py
+++ scripts/spatial_spectral_optics.py
@@ -2,12 +2,20 @@ import os
 import sys
 import argparse
 import logging
+import yaml
 import numpy as np
 import pandas as pd
 import xarray as xr
 from pprint import pprint
 
 from analysis_utils import fill_date_template
+
+
+def read_aerosol_optics(filename):
+    logging.info(filename)
+    with open(args.aerosol, 'r') as f:
+        aerosol_config = yaml.safe_load(f)
+        pprint(aerosol_config)
 
 
 def process_file(filename):
@@ -30,11 +38,14 @@ if __name__ == '__main__':
     parser.add_argument('--datadir', type=str,
         default=os.path.join(os.getenv('HOME'), 'Data'),
         help='top-level data directory (default $HOME/Data)')
+    parser.add_argument('--aerosol', type=str,
+        default=os.path.join('..', 'optics', 'aerosol.yaml'),
+        help='yaml aerosol file')
     parser.add_argument('--start', type=str,
         default='20080701',
         help='start date (yyyymmdd)')
     parser.add_argument('--end', type=str,
-        default='20080705',
+        default='20080701',
         help='end date (yyyymmdd)')
     parser.add_argument('--file_pattern', type=str,
         default=os.path.join('MERRA2_inst3', 'aer_Nv',
@@ -46,6 +57,8 @@ if __name__ == '__main__':
     """
     logging_level = logging.DEBUG if args.debug else logging.INFO
     logging.basicConfig(stream=args.logfile, level=logging_level)
+
+    read_aerosol_optics(args.aerosol)
 
     dates = pd.date_range(start=args.start, end=args.end, freq='D')
     logging.info(dates)
